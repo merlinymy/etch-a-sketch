030086f37b4cb1ba50d8f0f9dda4fbac1ecbed94
Merlin Yang <lordhorcrux@gmail.com>
2024-05-14 17:35:39 -0500

    Implement core functionality
    Hard coded color, number of grid.
    Also comfirmed general style of the page.

---
+++ javascript.js
@@ -0,0 +1,39 @@
+// get the div that small squares reside in
+const containerDiv = document.querySelector(".container");
+
+
+// user choose the number of grid (x by x)
+// container length / number of grid is the size of small div (pixel)
+let containerWidth = 750;
+
+function generateSquares(numberSquares) {
+    // we need to know the width/height of pixels
+    let pixelWidth = containerWidth / numberSquares;
+    console.log(pixelWidth);
+    for (let i = 0; i < numberSquares; i++) {
+        for (let j = 0; j < numberSquares; j++) {
+            const pixelSquare = document.createElement("div");
+            pixelSquare.style.height = `${pixelWidth}px`;
+            pixelSquare.style.width = `${pixelWidth}px`;
+            pixelSquare.style.border = "black solid 0.2px"
+            pixelSquare.className = "pixel";
+            containerDiv.appendChild(pixelSquare);
+        }    
+    }
+}
+
+function attachListeners(pixel) {
+    pixel.addEventListener("mouseover", (event) => {
+        paint(event);
+    });
+}
+
+function paint(event) {
+    const target = event.target;
+    target.style.backgroundColor = "red";
+}
+
+generateSquares(64);
+const pixelDivs = document.querySelectorAll(".pixel");
+// console.log(pixelDivs);
+pixelDivs.forEach(pixel => attachListeners(pixel));
